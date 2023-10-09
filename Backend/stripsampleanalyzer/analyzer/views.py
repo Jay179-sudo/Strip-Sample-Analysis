75d0c9b0b0656b3bb254b822a4b69326d2d39f89
Jay179-sudo <jayprasad2002@gmail.com>
2023-10-09 21:35:26 +0530

    Added opencv to application. Added task to analyzer view and changed settings for an environ misconfiguration

--- Backend/stripsampleanalyzer/analyzer/views.py
+++ Backend/stripsampleanalyzer/analyzer/views.py
@@ -2,13 +2,14 @@ from .serializers import AnalyzerSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
-
+from .tasks import create_task
 class AnalyzerView(APIView):
     serializer_class = AnalyzerSerializer
 
     def post(self, request, format=None):
         serializer = AnalyzerSerializer(data=request.data)
         if serializer.is_valid():
+            create_task.delay(email=request.data.get("email"),image=request.data.get("image").read())
             return    Response({"msg": "Request successful!"}, status=status.HTTP_201_CREATED, )
         return Response(
             {"msg": "Request unsuccessful"}, status=status.HTTP_400_BAD_REQUEST)
