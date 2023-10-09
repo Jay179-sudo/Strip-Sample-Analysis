aa47b385d2aa5678afd274d172d47ec42d2a6ab2
Jay179-sudo <jayprasad2002@gmail.com>
2023-10-09 20:32:21 +0530

    Initial folder structure and barebones frontend implemented. Better design required. Docker-compose file initialized.

---
+++ Frontend/script.js
@@ -0,0 +1,27 @@
+document
+  .getElementById("submitForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var email = document.getElementById("email").value;
+    var image = document.getElementById("image").files[0];
+
+    var formData = new FormData();
+    formData.append("email", email);
+    formData.append("image", image);
+
+    fetch("http://localhost:1337/home/", {
+      method: "POST",
+      body: formData,
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        // Handle the response from the server as needed
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  });
